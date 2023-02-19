4c06734534abdd97c9ce4cab36390504d6e329c9
gustavorsan <gustavorsantos42@gmail.com>
2023-02-19 18:10:54 -0300

    Resolução do exercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import model.Clinica;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Clinica clinica = new Clinica("Gustavo","D'Beleza","Rua Zero","SN");
+
+        System.out.println(clinica.toString());
     }
+
+
 }
