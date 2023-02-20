304e4fa01edd8ed1dfcbdb9c1e457591d67523d1
gustavorsan <gustavorsantos42@gmail.com>
2023-02-20 18:55:30 -0300

    adicionado dados faltantes do cliente e contrutor da classe

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -3,10 +3,27 @@ package model;
 public class Cliente {
     private String nome;
     private String dados;
+    private  int idade;
+
+    private double altura;
+    private double peso;
 
     public Cliente(String nome, String dados) {
         this.nome = nome;
         this.dados = dados;
+    }
+
+    public Cliente(double altura, double peso) {
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Cliente(String nome, String dados, int idade, double altura, double peso) {
+        this.nome = nome;
+        this.dados = dados;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
     }
 
     @Override
